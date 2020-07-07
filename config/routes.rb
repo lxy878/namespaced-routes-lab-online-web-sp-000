996274637540cce6b8911caeda02544448830b4f
Xiaoyi Liu <lxy878@gmail.com>
2020-07-07 15:50:29 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
 
-  get 'preference/allow_create_artists:boolean'
-
-  get 'preference/allow_create_songs:boolean'
-
   resources :artists do
     resources :songs, only: [:index, :show]
   end
